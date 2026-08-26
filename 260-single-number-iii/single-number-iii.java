03eac8e78951be2e3cac2d926af3556f2aef2bdb
sairaghunadh2006 <sairaghunadh2006@gmail.com>
2026-08-26 17:01:01 +0530

    Time: 1 ms (100.00%) | Memory: 48.2 MB (8.61%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int[] arr = new int[2];
-        int ind = 0;
-        HashMap<Integer,Integer> h = new HashMap<>();
+        int x = 0;
         for(int i = 0;i<nums.length;i++){
-            if(h.containsKey(nums[i])){
-                h.put(nums[i],h.get(nums[i]) + 1);
-            }
-            else{
-                h.put(nums[i],1);
-            }
+            x^=nums[i];
         }
-        for(int i : h.keySet()){
-            if(h.get(i) == 1) arr[ind++] = i;
+        int rm = (x^(x - 1))&x;
+        int b1 = 0;
+        int b2 = 0;
+        for(int i = 0;i<nums.length;i++){
+            if((nums[i]&rm) != 0) b1^=nums[i];
+            else b2^=nums[i];
         }
+        int[] arr = {b1,b2};
         return arr;
     }
 }
